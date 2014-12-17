8c8f743518ce20c29fb31e70f92fb0df5413d251
weimeidsg <359593891@qq.com>
2014-12-17 13:49:44 +0100

    platform

---
+++ Share/Scripts/VirtualPlatform.js
@@ -0,0 +1,10 @@
+﻿
+function OnTriggerEnter2D (jumper: Collider2D) {
+    var platform = transform.parent;
+    Physics2D.IgnoreCollision(jumper.GetComponent(BoxCollider2D), platform.GetComponent(BoxCollider2D));
+}
+ 
+function OnTriggerExit2D (jumper: Collider2D) {
+    var platform = transform.parent;
+    Physics2D.IgnoreCollision(jumper.GetComponent(BoxCollider2D), platform.GetComponent(BoxCollider2D), false);
+}
