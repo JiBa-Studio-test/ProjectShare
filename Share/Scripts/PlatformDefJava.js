42d75ad454ff1ad8190c32b3e597e70cf30e2983
weimeidsg <359593891@qq.com>
2015-01-02 20:19:11 +0100

    moving platform

---
+++ Share/Scripts/PlatformDefJava.js
@@ -0,0 +1,14 @@
+﻿var points: Transform[];
+var length:int;
+var index:int;
+function OnDrawGizmos()
+{
+	if (points == null || points.Length < 2) 
+	{
+		return;
+	}
+	for(var i=1;i<points.Length;i++)
+	{
+		Gizmos.DrawLine(points[i-1].position,points[i].position);
+	}
+}
